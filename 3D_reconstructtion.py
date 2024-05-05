98db8689221c390b5ff8925006ab8e8822595550
曹銨文 <90565418+109302013evantsao@users.noreply.github.com>
2024-05-06 00:11:29 +0800

    first test

---
+++ 3D_reconstructtion.py
@@ -0,0 +1,6 @@
+# This is a project a about 3D reconstruction of a scene from a set of 2D images.
+
+# First, get the camera parameters from the set of images.
+# to get the camera intrinsic matrix, we need to get the focal length(fx, fy) and the optical point(cx, cy).
+
+
